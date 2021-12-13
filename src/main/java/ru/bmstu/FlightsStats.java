2a54710dbc88cb5b8df2bd90665e6948c263a772
Alexey Savin <therealsava@icloud.com>
2021-12-14 00:56:00 +0300

    Scripted auto-commit on change (2021-12-14 00:56:00) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightsStats.java
+++ src/main/java/ru/bmstu/FlightsStats.java
@@ -3,5 +3,13 @@ package ru.bmstu;
 import java.io.Serializable;
 
 public class FlightsStats implements Serializable {
-    private int count, countCancelledFlights, 
+    private int count, countCancelledFlights, countLateFlights;
+    private float maxDelay;
+
+    public FlightsStats (int count, int countCancelledFlights, int countLateFlights, float maxDelay) {
+        this.count = count;
+        this.countCancelledFlights = countLateFlights;
+        this.countLateFlights = countLateFlights;
+        this.maxDelay = maxDelay;
+    }
 }
