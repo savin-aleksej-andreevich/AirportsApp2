de6753bf9c40a3089098bda2c9841f90d534d7b6
Alexey Savin <therealsava@icloud.com>
2021-12-14 00:07:43 +0300

    Scripted auto-commit on change (2021-12-14 00:07:43) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightParser.java
+++ src/main/java/ru/bmstu/FlightParser.java
@@ -13,10 +13,10 @@ public class FlightParser {
     public static Tuple2<Tuple2<Integer, Integer>, Flight> parseFlightsFile(String row) {
         String[] params = row.split(SEPARATOR);
         int originAirportID = Integer.parseInt(params[ORIGIN_AIRPORT_INDEX]);
-        int destAirportId = Integer.parseInt(params[DEST_AIRPORT_INDEX]);
+        int destAirportID = Integer.parseInt(params[DEST_AIRPORT_INDEX]);
         float delay = params[DELAYTIME_INDEX].isEmpty()? 0.f : Float.parseFloat(params[DELAYTIME_INDEX]);
         float isCanceled = Float.parseFloat(params[CANCELLED_INDEX]);
-        return new Tuple2<>(new Tuple2<>(originAirportId, destAirportId), new Flight(destAirportId, delay, isCanceled));
+        return new Tuple2<>(new Tuple2<>(originAirportId, destAirportId), new Flight(originAirportID, destAirportId, delay, isCanceled));
     }
 
     public static boolean isNotFlightsHeader(String row){
