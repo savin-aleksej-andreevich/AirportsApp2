c96165ff90d40df19b1c9ba79dcc56bb6e107fbe
Alexey Savin <therealsava@icloud.com>
2021-12-13 23:36:07 +0300

    Scripted auto-commit on change (2021-12-13 23:36:06) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsApp2.java
+++ src/main/java/ru/bmstu/AirportsApp2.java
@@ -10,7 +10,9 @@ import java.util.Map;
 
 
 public class AirportsApp2 {
+    
     public static void main(String[] args) {
-        
+        SparkConf conf = new SparkConf().setAppName("lab5");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
