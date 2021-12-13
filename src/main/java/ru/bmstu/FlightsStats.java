b9ad602e2bf0677c63a86bce6dba5bc4b47844a4
Alexey Savin <therealsava@icloud.com>
2021-12-14 00:52:14 +0300

    Scripted auto-commit on change (2021-12-14 00:52:14) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightsStats.java
+++ src/main/java/ru/bmstu/FlightsStats.java
@@ -2,6 +2,6 @@ package ru.bmstu;
 
 import java.io.Serializable;
 
-public class FlightsStats extends Serializable{
-
+public class FlightsStats implements Serializable {
+    private int count, 
 }
