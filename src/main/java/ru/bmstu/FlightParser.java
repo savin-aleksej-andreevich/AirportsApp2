132633a698f9b1f9c18b74b3659e0709b4d3a6c4
Alexey Savin <therealsava@icloud.com>
2021-12-13 23:45:02 +0300

    Scripted auto-commit on change (2021-12-13 23:45:02) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightParser.java
+++ src/main/java/ru/bmstu/FlightParser.java
@@ -1,5 +1,12 @@
 package ru.bmstu;
 
 public class FlightParser {
-    pu
+    public static int DEST_AIRPORT_INDEX = 14;
+    public static int ORIGIN_AIRPORT_INDEX = 11;
+    public static int DELAYTIME_INDEX = 18;
+    public static int CANCELLED_INDEX = 19;
+    public static String FLIGHT_DELIMETR = ",";
+    public static String FLIGHTS_HEADER_SUBSTR = "YEAR";
+
+    public 
 }
