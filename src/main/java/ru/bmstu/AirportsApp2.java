249eef23322b7d57ce9ad5a30ba5b10cb06989ae
Alexey Savin <therealsava@icloud.com>
2021-12-14 00:20:03 +0300

    Scripted auto-commit on change (2021-12-14 00:20:03) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsApp2.java
+++ src/main/java/ru/bmstu/AirportsApp2.java
@@ -11,7 +11,7 @@ public class AirportsApp2 {
     public static String outputPath = "hdfs://localhost:9000/user/sava/output_3";
 
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("lab5");
+        SparkConf conf = new SparkConf().setAppName("AirportsApp2");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> rows = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaPairRDD<Tuple2<Integer, Integer>, Flights> flights = rows.filter(FlightsParser::isNotFlightsHeader)
