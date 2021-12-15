e3218790213008e7e468bf7c13d9b185853f47b4
Alexey Savin <therealsava@icloud.com>
2021-12-15 22:14:26 +0300

    Scripted auto-commit on change (2021-12-15 22:14:26) by gitwatch.sh

--- src/main/java/ru/bmstu/FlightStats.java
+++ src/main/java/ru/bmstu/FlightStats.java
@@ -18,14 +18,19 @@ public class FlightStats implements Serializable {
     }
 
     public static FlightStats addFlight (FlightStats stats, Flight flight) {
+        int count = stats.getCount() + 1;
         int countCancelledFlights = flight.getIsCanceled() == 0.f? stats.getCountCancelledFlights() + 1 : stats.getCountCancelledFlights();
         int countLateFlights = flight.getDelay() == 0.f? stats.getCountLateFlights() + 1 : stats.getCountLateFlights();
         float maxLateTime = Math.max(stats.getMaxLateTime(), flight.getDelay());
-        return new FlightStats(stats.getCount() + 1, countCancelledFlights, countLateFlights, maxLateTime);
+        return new FlightStats(count, countCancelledFlights, countLateFlights, maxLateTime);
     }
 
     public static FlightStats combineFlightStats (FlightStats stats1, FlightStats stats2) {
         int combineCount = stats1.getCount() + stats2.getCount();
+        int countCancelledFlights = stats1.getCountCancelledFlights() + stats2.getCountCancelledFlights();
+        int countLateFlights = stats1.getCountLateFlights() + stats2.getCountLateFlights();
+        float maxLateTime = Math.max(stats1.getMaxLateTime(), stats2.getMaxLateTime());
+        return new FlightStats(combineCount, countCancelledFlights, countLateFlights, maxLateTime);
     }
 
     public int getCount() {
