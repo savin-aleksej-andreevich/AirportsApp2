be7eb37ffc07cb57354986edf020eee957d4b7a6
Alexey Savin <therealsava@icloud.com>
2021-12-15 21:52:54 +0300

    Scripted auto-commit on change (2021-12-15 21:52:54) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsApp2.java
+++ src/main/java/ru/bmstu/AirportsApp2.java
@@ -5,7 +5,10 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
-
+/*
+Требуется определить для пары <аэропорт отлета, аэропорт прибытия> максимальное время опоздания, процент опоздавших+отмененных рейсов.
+Также требуется связать полученную таблицу с названиями аэропортов.
+ */
 
 public class AirportsApp2 {
     public static String outputPath = "hdfs://localhost:9000/user/sava/output_3";
