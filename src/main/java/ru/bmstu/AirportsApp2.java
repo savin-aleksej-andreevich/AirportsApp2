509df469d860e3903fe436fb6ec8be84133af4be
Alexey Savin <therealsava@icloud.com>
2021-12-15 22:31:19 +0300

    Scripted auto-commit on change (2021-12-15 22:31:19) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsApp2.java
+++ src/main/java/ru/bmstu/AirportsApp2.java
@@ -4,6 +4,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.broadcast.Broadcast;
 import scala.Tuple2;
 
 import java.util.Map;
