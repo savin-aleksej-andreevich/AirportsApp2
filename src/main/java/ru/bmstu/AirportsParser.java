0a561ce2ff6441d24eab735aeb5cf8b41930a5d4
Alexey Savin <therealsava@icloud.com>
2021-12-15 22:24:56 +0300

    Scripted auto-commit on change (2021-12-15 22:24:56) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsParser.java
+++ src/main/java/ru/bmstu/AirportsParser.java
@@ -1,7 +1,15 @@
 package ru.bmstu;
 
 public class AirportsParser {
+    public static String AIRPORT_ROW_DELIMETR = "\",\"";
+    public static String AIRPORT_DELIMETR = "\"";
+    public static int AIRPORT_ID_INDEX = 0;
+    public static int AIRPORT_NAME_INDEX = 1;
+    public static String AIRPORT_HEADER_SUBSTR = "Code";
+    public static int AIRPORT_NAME_WITHOUT_QUITE_INDEX = 0;
+    public static int AIRPORT_ID_WITHOUT_QUITE_INDEX = 1;
+
     public static boolean isNotAirportsHeader(String row){
-        return !row.contains(FLIGHTS_HEADER_SUBSTR);
+        return !row.contains(AIRPORT_HEADER_SUBSTR);
     }
 }
