7978aa701ce46a325d44cde895fc440edc5ddbe1
Alexey Savin <therealsava@icloud.com>
2021-12-15 22:28:34 +0300

    Scripted auto-commit on change (2021-12-15 22:28:34) by gitwatch.sh

--- src/main/java/ru/bmstu/AirportsParser.java
+++ src/main/java/ru/bmstu/AirportsParser.java
@@ -1,8 +1,10 @@
 package ru.bmstu;
 
+import scala.Tuple2;
+
 public class AirportsParser {
-    public static String AIRPORT_ROW_DELIMETR = "\",\"";
-    public static String AIRPORT_DELIMETR = "\"";
+    public static String AIRPORT_ROW_SEPARATOR = "\",\"";
+    public static String AIRPORT_SEPARATOR = "\"";
     public static int AIRPORT_ID_INDEX = 0;
     public static int AIRPORT_NAME_INDEX = 1;
     public static String AIRPORT_HEADER_SUBSTR = "Code";
@@ -13,5 +15,10 @@ public class AirportsParser {
         return !row.contains(AIRPORT_HEADER_SUBSTR);
     }
 
-    public static 
+    public static Tuple2<Integer, String> parseAiportFile(String row){
+        String[] params = row.split(AIRPORT_ROW_SEPARATOR);
+        int airportId = Integer.parseInt(params[AIRPORT_ID_INDEX].split(AIRPORT_SEPARATOR)[AIRPORT_ID_WITHOUT_QUITE_INDEX]);
+        String airportName = params[AIRPORT_NAME_INDEX].split(AIRPORT_SEPARATOR)[AIRPORT_NAME_WITHOUT_QUITE_INDEX];
+        return new Tuple2<>(airportId, airportName);
+    }
 }
